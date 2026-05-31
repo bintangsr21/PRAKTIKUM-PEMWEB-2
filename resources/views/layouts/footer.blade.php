0ada00406933c545a8cff7cf2b77e1a31689875a
bintangsr21 <bintangsr1011@gmail.com>
2026-06-01 00:00:35 +0700

    feat: Update content for branch PERTEMUAN-9

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="alert alert-primary text-center" role="alert">
+  <strong>Pemrograman Web 2</strong> <br>
+  <small>Semester Genap</small>
+</div>
+
+<footer class="text-center mt-4">
+  <hr>
+  <p class="mb-1">Dosen Pengampu: Pak Nasrul, S.Kom., M.Kom.</p>
+  <p class="mb-1">Nama: AZI SYAHRIJAL | NIM: 0110225128 | Prodi: TEKNIK INFORMATIKA</p>
+  <small class="text-muted">© 2026 STT Terpadu Nurul Fikri</small>
+</footer>
